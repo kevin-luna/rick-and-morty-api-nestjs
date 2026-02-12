3759f71743ca2475ebe16a8fc883cee9405d5965
kevin-luna <L21020397@veracruz.tecnm.mx>
2026-02-12 17:15:39 -0600

    Se creó un método para filtrar los personajes

--- src/characters/characters.service.ts
+++ src/characters/characters.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Character, CharacterPage } from './characters.dto';
+import { CharactersRepository } from './characters.repository';
 
 @Injectable()
-export class CharactersService {}
+export class CharactersService {
+
+  charactersRepository:CharactersRepository;
+
+  constructor(charactersRepository: CharactersRepository){
+      this.charactersRepository = charactersRepository;
+  }
+
+  async getAliveCharactersFormatted(){
+    const allCharacters = await this.charactersRepository.getAllCharacters();
+    return allCharacters.filter((character) => character.status=='alive').map((character)=> character.name.replaceAll(' ','_'))
+  }
+
+}
