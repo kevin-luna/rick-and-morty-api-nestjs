ec6bf45af44d25e813a3aec5397bbf6a552e4ddb
kevin-luna <L21020397@veracruz.tecnm.mx>
2026-02-12 18:42:42 -0600

    Se agrego el controller characters al modulo principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CharactersModule } from './characters/characters.module';
 import { CharactersController } from './characters/characters.controller';
+import { CharactersService } from './characters/characters.service';
+import { CharactersRepository } from './characters/characters.repository';
 
 @Module({
   imports: [CharactersModule],
   controllers: [AppController,CharactersController],
-  providers: [AppService],
+  providers: [AppService, CharactersService, CharactersRepository],
 })
 export class AppModule {}
--- src/characters/characters.controller.ts
+++ src/characters/characters.controller.ts
@@ -1,7 +1,19 @@
-import {Controller, Get} from '@nestjs/common'
+import {Controller, Get, Query} from '@nestjs/common'
 import { CharactersService } from './characters.service'
 
 @Controller({})
 export class CharactersController{
+    charactersService: CharactersService;
+
+    constructor(charactersService: CharactersService){
+        this.charactersService = charactersService;
+    }
+
+    @Get('characters/')
+    async getCharacters(@Query('page') page: number){
+        const response = await this.charactersService.getAliveCharactersFormatted(page);
+        console.log(response)
+        return response
+    }
 
 }
